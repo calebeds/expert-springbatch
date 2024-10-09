39a27386865c24cd9758532c4c3cb12059624547
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2024-10-08 21:34:13 -0300

    add paritioner

---
+++ src/main/java/me/calebe_oliveira/expertspringbatchapp/partioners/SessionActionPartitioner.java
@@ -0,0 +1,28 @@
+package me.calebe_oliveira.expertspringbatchapp.partioners;
+
+import org.springframework.batch.core.partition.support.Partitioner;
+import org.springframework.batch.item.ExecutionContext;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SessionActionPartitioner implements Partitioner {
+    public static final String PARTITION_COUNT = "partitionCount";
+    public static final String PARTITION_INDEX = "partitionIndex";
+    private static final String PARTITION_NAME_PREFIX = "sessionActionPartition-";
+
+
+    @Override
+    public Map<String, ExecutionContext> partition(int gridSize) {
+        Map<String, ExecutionContext> partitionMap = new HashMap<>(gridSize);
+
+        for (int i = 0; i < gridSize; i++) {
+            ExecutionContext executionContext = new ExecutionContext();
+            executionContext.putInt(PARTITION_COUNT, gridSize);
+            executionContext.putInt(PARTITION_INDEX, i);
+            partitionMap.put(PARTITION_INDEX + i, executionContext);
+        }
+
+        return partitionMap;
+    }
+}
